0cddeee3569abc901723670422609b2b0d84bccd
ArielMakerLabPy <arielmakerlab@outlook.com>
2020-10-26 09:11:32 -0300

    Realizando tables

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import "./style.css";
 
 // Write Javascript code!
 const appDiv = document.getElementById("app");
-appDiv.innerHTML = `<h1>Empezamo con Javascript<br /> - <br /><strong> By Ariel</strong></h1>
+appDiv.innerHTML = `<h1>Realizamos una página web<br /> - <br /><strong> By Ariel</strong></h1>
 <h2>FIN</h2>`;
